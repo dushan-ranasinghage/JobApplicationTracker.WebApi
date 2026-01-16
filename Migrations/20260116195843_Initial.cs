8728ef93d246cb9320192099d3a1a84e6569d495
Dushan Ranasinghage <dushanrandika1994@outlook.com>
2026-01-17 09:00:49 +1300

    Add the initial migration to create JobApplication table in sqlite db

---
+++ Migrations/20260116195843_Initial.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobApplicationTracker.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JobApplications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CompanyName = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Position = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    DateApplied = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobApplications", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobApplications");
+        }
+    }
+}
